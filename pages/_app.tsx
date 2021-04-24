1c51ae016d304fb85a35d04679b7b31b4a52ac7d
shibayu36 <shibayu36@gmail.com>
2021-04-24 16:11:11 +0900

    queryClientでwrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import { Provider } from "next-auth/client";
 import type { AppProps } from "next/app";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps): React.ReactElement {
   return (
-    <Provider session={pageProps.session}>
-      <Component {...pageProps} />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider session={pageProps.session}>
+        <Component {...pageProps} />
+      </Provider>
+    </QueryClientProvider>
   );
 }
