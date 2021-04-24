9b9c53c049049029df41fe81b55054191c0c883a
shibayu36 <shibayu36@gmail.com>
2021-04-24 16:06:55 +0900

    TODOを作成するエンドポイント

--- pages/api/todos.ts
+++ pages/api/todos.ts
@@ -2,6 +2,11 @@ import { PrismaClient } from "@prisma/client";
 import { getSession } from "next-auth/client";
 import { NextApiHandler } from "next-auth/internals/utils";
 
+type CreateTodoData = Readonly<{
+  title: string;
+  body: string;
+}>;
+
 const prisma = new PrismaClient();
 
 const todosHandler: NextApiHandler = async (req, res) => {
@@ -16,6 +21,24 @@ const todosHandler: NextApiHandler = async (req, res) => {
       where: { userId },
     });
     return res.status(200).json(todos);
+  }
+
+  if (req.method === "POST") {
+    const { title, body } = JSON.parse(req.body) as CreateTodoData;
+
+    const createdTodo = await prisma.todo.create({
+      data: {
+        title,
+        body,
+        User: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+
+    res.status(201).json(createdTodo);
   }
 };
 
